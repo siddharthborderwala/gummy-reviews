575d95cf752a8fe1db19d88ce80e8bd2de48f73c
Siddharth Borderwala <siddharthborderwala@gmail.com>
2021-12-09 16:50:43 +0530

    update modal

--- client/src/components/modal/index.js
+++ client/src/components/modal/index.js
@@ -3,6 +3,10 @@ import { destroy, render } from '../../lib/util';
 import Button from '../button';
 import './styles.css';
 
+const X = `
+<svg xmlns="http://www.w3.org/2000/svg" width="20" height="20" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="feather feather-x"><line x1="18" y1="6" x2="6" y2="18"></line><line x1="6" y1="6" x2="18" y2="18"></line></svg>
+`;
+
 class Modal extends Component {
   constructor(props) {
     super(props);
@@ -24,7 +28,7 @@ class Modal extends Component {
         <div class="modal--container">
           <div class="modal--cross-btn">
           ${new Button({
-            label: 'x',
+            label: X,
             onclick: {
               target: 'self',
               callback: () => {
