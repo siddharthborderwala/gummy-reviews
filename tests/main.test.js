eb31e8cf0e6b9e9554d4ac35a1dbb2a1c0320d6b
Siddharth Borderwala <siddharthborderwala@gmail.com>
2021-12-09 19:43:33 +0530

    setup and create tests

---
+++ tests/main.test.js
@@ -0,0 +1,38 @@
+const supertest = require('supertest');
+
+const app = require('../src/app');
+const server = supertest(app);
+
+describe('Test main GET endpoints', () => {
+  let defaultProductId;
+
+  it('GET /products/default/ should default product', async () => {
+    const res = await server.get('/api/v1/products/default');
+    expect(res.status).toEqual(200);
+    expect(res.type).toEqual(expect.stringContaining('json'));
+    expect(res.body).toHaveProperty('product');
+    const product = res.body.product;
+    expect(product).toHaveProperty('id');
+    defaultProductId = product.id;
+    expect(product).toHaveProperty('name');
+    expect(product).toHaveProperty('averageRating');
+  });
+
+  it('GET /products/:productId/reviews/ should show product reviews', async () => {
+    const res = await server.get(
+      `/api/v1/products/${defaultProductId}/reviews`
+    );
+    expect(res.status).toEqual(200);
+    expect(res.type).toEqual(expect.stringContaining('json'));
+    expect(res.body).toHaveProperty('reviews');
+    expect(res.body.reviews).toBeDefined();
+  });
+
+  it('GET /reviews/product/:productId should show product reviews', async () => {
+    const res = await server.get(`/api/v1/reviews/product/${defaultProductId}`);
+    expect(res.status).toEqual(200);
+    expect(res.type).toEqual(expect.stringContaining('json'));
+    expect(res.body).toHaveProperty('reviews');
+    expect(res.body.reviews).toBeDefined();
+  });
+});
